8c8aa03483e6dcd9012624652e301ad22e17ca85
AlexM <alexm200164@gmail.com>
2020-11-19 18:52:17 +0100

    Cambio de IDS
    He cambiado todas las ID y actualizado la versión.
    
    AVISO, SI VAS A GENERAR UN NUEVO ELEMENTO GRÁFICO O CAMBIAS ID'S DE OTROS ELEMENTOS SE DEBE AVISAR CON ANTELACIÓN Y PLANIFICAR EL TRASPASO PARA NO INCURRIR EN PROBLEMAS DE VERSIONES Y NOMENCLATURAS.

--- InicioSesion.vb
+++ InicioSesion.vb
@@ -3,7 +3,11 @@
 
     End Sub
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label_usuario.Click
+
+    End Sub
+
+    Private Sub PantallaVentas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
